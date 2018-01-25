a783f15023f9557788b22ac748de66298601b51c
Subham <subha7755m@gmail.com>
2018-01-25 10:40:19 +0530

    Remove client side setup

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.set('port', 3000);
 
-var server = app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
